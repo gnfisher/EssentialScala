bc86f43615c4cc2182b274cf4bbf4e7b86c0d18e
Greg Fisher <gnfisher@thoughtbot.com>
2020-02-19 23:00:44 -0500

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,4 @@
+name := "essential"
+organization := "g.f"
+version := "0.1-SNAPSHOT"
+scalaVersion := "2.12.8"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,17 @@
+object Scala {
+  def main(args: Array[String]): Unit = {
+    val list = Pair(1, Pair(1, Pair(1, End)))
+    assert(list.sum == 3)
+  }
+
+  sealed trait IntList {
+    def sum: Int =
+      this match {
+        case End => 0
+        case Pair(num, tail) =>
+          num + tail.sum
+      }
+  }
+  case object End extends IntList
+  final case class Pair(head: Int, tail: IntList) extends IntList
+}
