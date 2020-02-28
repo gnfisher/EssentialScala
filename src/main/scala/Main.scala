40691719816657555c6af9702d4b607f82a8252b
Greg Fisher <gnfisher@thoughtbot.com>
2020-02-28 10:53:34 -0500

    Sum a tree

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -5,6 +5,8 @@ object Scala {
     val list = Pair(1, Pair(1, Pair(1, End)))
     assert(list.sum == 3)
 
+    val tree = Node(Leaf(1), Node(Leaf(2), Leaf(3)))
+    assert(tree.sum == 6)
   }
 
   sealed trait IntList {
@@ -17,4 +19,15 @@ object Scala {
   }
   case object End extends IntList
   final case class Pair(head: Int, tail: IntList) extends IntList
+
+  sealed trait Tree {
+    def sum: Int =
+      this match {
+        case Leaf(element) => element
+        case Node(l, r) =>
+          l.sum + r.sum
+      }
+  }
+  final case class Node(left: Tree, right: Tree) extends Tree
+  final case class Leaf(element: Int) extends Tree
 }
